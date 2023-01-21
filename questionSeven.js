d4960f70ca6433c1788749b841215a33d706c728
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-21 22:10:45 +0530

    added question seven

---
+++ questionSeven.js
@@ -0,0 +1,9 @@
+var loadText = document.querySelector("#txt-load");
+var btnLoad = document.querySelector("#btn-load");
+
+
+btnLoad.addEventListener("click",function(){
+    // loadText.style.color = "white";
+    // loadText.innerHTML =  "";
+    loadText.style.display = "none";
+})
