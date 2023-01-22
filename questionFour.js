7d98f22741ee558d4dee208b3d0e10d3cecffdc2
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-22 18:15:31 +0530

    modified question4 solution

--- questionFour.js
+++ questionFour.js
@@ -1,4 +1,5 @@
-var txt = document.querySelector("#txt");
+var inp = document.querySelector("#inp");
+var inpText = document.querySelector("#inp-txt");
 var hone = document.querySelector("#h-one");
 var htwo = document.querySelector("#h-two");
 var hthree = document.querySelector("#h-three");
@@ -8,13 +9,17 @@ var h1 = 24;
 var h2 = 22;
 var h3 = 20;
 hone.addEventListener("click",function(){
-    txt.style.fontSize = h1+"px";
+    inpText.style.fontSize = h1+"px";
 })
 
 htwo.addEventListener("click",function(){
-    txt.style.fontSize = h2+"px";
+    inpText.style.fontSize = h2+"px";
 })
 
 hthree.addEventListener("click",function(){
-    txt.style.fontSize = h3+"px";
+    inpText.style.fontSize = h3+"px";
+})
+
+inp.addEventListener('change',function(){
+    inpText.innerHTML = inp.value;
 })
