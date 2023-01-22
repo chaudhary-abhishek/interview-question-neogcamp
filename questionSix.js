7d3b498983cfbdda19010900655c7c504ddb3e78
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-22 18:32:13 +0530

    modified question 6 solution

--- questionSix.js
+++ questionSix.js
@@ -1,22 +1,23 @@
-const readFileSync = require('readline-sync');
+const readLineSync = require('readline-sync');
 
 var students=[];
 var maxMarks = 0;
 var maxMarkStudent = "";
 for(var i=0;i<2;i++){
-    var unitMarks = readFileSync.question("Please enter your unit exam marks: ");
-    var preMarks = readFileSync.question("Please enter your pre exam marks: ");
-    var mainMarks = readFileSync.question("Please enter your main exam marks: ");
+    var name = readLineSync.question("Please give me your name: ")
+    var unitMarks = readLineSync.question("Please enter your unit exam marks: ");
+    var preMarks = readLineSync.question("Please enter your pre exam marks: ");
+    var mainMarks = readLineSync.question("Please enter your main exam marks: ");
     students[i] = parseInt(unitMarks)+parseInt(preMarks)+parseInt(mainMarks);
     if(students[i]>maxMarks){
         maxMarks = students[i];
-        maxMarkStudent = i;
+        maxMarkStudent = name;
     }
 
     console.log("-----------------------------------------------");
 }
 
-console.log("The Student: "+i+" got the maximum marks, he/she got: "+maxMarks);
+console.log("The Student: "+maxMarkStudent+" got the maximum marks, he/she got: "+maxMarks);
 
-console.log("The Student: "+i+" has the average marks: "+maxMarks/3);
+console.log("The Student: "+maxMarkStudent+" has the average marks: "+maxMarks/3);
 
