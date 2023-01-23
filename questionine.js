33705380a10b9a5c8cb033264f665760fa832eae
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-23 23:08:59 +0530

    questionNine solution

---
+++ questionine.js
@@ -0,0 +1,19 @@
+
+var inpText = document.querySelector('#inp-txt');
+var textArea = document.querySelector('#txt-area');
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.jso";
+
+function urlGenerator(text){
+    return url+"?text="+text;
+}
+
+function errorHandler(e){
+    console.log("Some error is there"+e);
+    textArea.value = e;
+    // alert("Something went wrong with server");
+}
+
+inpText.addEventListener('change',function Urlfetch(){
+    fetch(urlGenerator(inpText.value)).then(response=>response.json()).then(jsonResponse=>textArea.value=jsonResponse.contents.translated)
+    .catch(errorHandler);
+})
